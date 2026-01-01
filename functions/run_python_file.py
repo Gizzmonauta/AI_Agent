8590f7e2d3c2e9a5512483f25a6523bb2e616f90
Gizzmonauta <gizzmonauta@hotmail.com>
2026-01-01 03:42:18 -0500

    halfway through assignment Run Python

---
+++ functions/run_python_file.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=None):
+    try:
+        # Resolve absolute paths
+        working_dir_abs = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
+
+        # Will be True or False
+        valid_target_file = os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
+
+        # Validate the target file is within the working directory
+        if not valid_target_file:
+            raise Exception(f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory')
+        
+        # Check if the target file exists and is a file
+        if not os.path.isfile(target_file):
+            raise Exception(f'Error: "{file_path}" does not exist or is not a regular file') 
+        
+        # Check if the file has a .py extension
+        if not target_file.endswith('.py'):
+            raise Exception(f'Error: "{file_path}" is not a Python file')
+        
+        # Prepare the command to run the Python file
+        command = ["python", target_file]
+        if args is not None:
+            command.extend(args)
+
+
+
+    except Exception as e:
+        return str(e)
+    
+    return content    
